7e1e044a938b3d000833a9afc1245eec8e418f40
gbriyad <gbriyad@gmail.com>
2020-08-13 00:43:35 +0600

    taxon and taxonomy seed added and homepage tanox links fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,23 @@ Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
 
 # create Relation Type for related product
 Spree::RelationType.create(name: 'Related Product', applies_to: 'Spree::Product')
+
+#taxonomy creation
+taxonomies = [
+    { name: I18n.t('spree.taxonomy_categories_name') }
+]
+
+taxonomies.each do |taxonomy_attrs|
+  Spree::Taxonomy.where(taxonomy_attrs).first_or_create!
+end
+
+#taxon creation
+categories = Spree::Taxonomy.find_by!(name: I18n.t('spree.taxonomy_categories_name'))
+categories_taxon = Spree::Taxon.where(name: I18n.t('spree.taxonomy_categories_name')).first_or_create!
+
+['Bestsellers', 'Trending', 'Offers', 'Sale'].each do |taxon_name|
+  taxon = categories_taxon.children.where(name: taxon_name).first_or_create!
+  taxon.permalink = taxon.permalink.gsub('categories/', '')
+  taxon.taxonomy = categories
+  taxon.save!
+end
