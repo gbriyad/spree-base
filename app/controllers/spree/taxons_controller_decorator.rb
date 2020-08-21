8955f2239c0eb9d479cab22c15ddb7a6825a4374
gbriyad <gbriyad@gmail.com>
2020-08-22 00:55:01 +0600

    revert test for cache issue in production for home taxon carausel

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -1,16 +1,16 @@
-module Spree
-  module TaxonsControllerDecorator
-    def product_carousel
-      #if stale?(etag: carousel_etag, last_modified: last_modified, public: true)
-        load_products
-        if @products.any?
-          render template: 'spree/taxons/product_carousel', layout: false
-        else
-          head :no_content
-        end
-      #end
-    end
-  end
-end
-
-Spree::TaxonsController.prepend Spree::TaxonsControllerDecorator if Spree::TaxonsController.included_modules.exclude?(Spree::TaxonsControllerDecorator)
+# module Spree
+#   module TaxonsControllerDecorator
+#     def product_carousel
+#       if stale?(etag: carousel_etag, last_modified: last_modified, public: true)
+#         load_products
+#         if @products.any?
+#           render template: 'spree/taxons/product_carousel', layout: false
+#         else
+#           head :no_content
+#         end
+#       end
+#     end
+#   end
+# end
+#
+# Spree::TaxonsController.prepend Spree::TaxonsControllerDecorator if Spree::TaxonsController.included_modules.exclude?(Spree::TaxonsControllerDecorator)
