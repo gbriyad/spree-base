af30963829683bb8e469a02d3bf805652d62052c
gbriyad <gbriyad@gmail.com>
2020-08-30 22:47:09 +0600

    order email for owner fixed

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -4,7 +4,7 @@ module Spree
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       current_store = @order.store
       subject = Spree.t('order_mailer.store_owner_notification_email.subject', store_name: current_store.name)
-      mail(to: current_store.mail_from_address, from: from_address, subject: subject)
+      mail(to: current_store.order_notification_email.split(','), from: from_address, subject: subject)
     end
   end
 end
