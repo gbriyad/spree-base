aa92c58a787a4751babd5938eb0b85a17a7214d2
gbriyad <gbriyad@gmail.com>
2020-09-10 15:24:57 +0600

    discount tag added over product image except product show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def generate_public_url(blob)
     ActiveStorage::Blob.service.path_for(blob.key).split('/public').second
   end
+
+  def product_discount_percentage(product)
+    if product.on_sale?
+      "-#{((product.original_price-product.price)/product.original_price*100).to_i}%"
+    else
+      "0%"
+    end
+  end
 end
